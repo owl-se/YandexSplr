eafd8b441aa5bb004aa42e2b66b0030c917e6762
owl-se <alloc89@mail.ru>
2019-04-18 20:22:14 +0400

    go on

--- src/main/java/speller/core/YandexSpellerApi.java
+++ src/main/java/speller/core/YandexSpellerApi.java
@@ -1,6 +1,23 @@
 package speller.core;
 
+import beans.YandexSpellerAnswer;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.apache.http.HttpStatus;
+import speller.utils.SpellerConstants;
+
 import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+import static org.hamcrest.Matchers.lessThan;
 
 public class YandexSpellerApi {
 
@@ -8,6 +25,62 @@ public class YandexSpellerApi {
 
     private HashMap<String, String> params = new HashMap<String, String>();
 
-    
+    public static class ApiBuilder {
+        YandexSpellerApi yandexSpellerApi;
 
+        private ApiBuilder(YandexSpellerApi api) {
+            yandexSpellerApi = api;
+        }
+
+        public ApiBuilder text(String text) {
+            yandexSpellerApi.params.put(SpellerConstants.PARAM_TEXT, text);
+            return this;
+        }
+
+        public ApiBuilder option(String option) {
+            yandexSpellerApi.params.put(SpellerConstants.PARAM_OPTION, option);
+            return this;
+        }
+
+        public ApiBuilder language(String lang) {
+            yandexSpellerApi.params.put(SpellerConstants.PARAM_LANG, lang);
+            return this;
+        }
+
+        public Response callApi() {
+            return RestAssured.with()
+                    .queryParam(yandexSpellerApi.params.toString())
+                    .log().all()
+                    .get(SpellerConstants.YANDEX_SPELLER_API_URI).prettyPeek();
+        }
+    }
+
+    public static ApiBuilder with() {
+        YandexSpellerApi api = new YandexSpellerApi();
+        return new ApiBuilder(api);
+    }
+
+   public static List<YandexSpellerAnswer> getYandexSpellerAnswer(Response response) {
+        return new Gson().fromJson(response.asString().trim(),
+                new TypeToken<List<YandexSpellerAnswer>>(){}.getType());
+   }
+
+   public static ResponseSpecification succesResponse() {
+        return new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .expectHeader("Connection", "keep-alive")
+                .expectResponseTime(lessThan(20000L))
+                .expectStatusCode(HttpStatus.SC_OK)
+                .build();
+   }
+
+   public static RequestSpecification baseRequestConf() {
+        return new RequestSpecBuilder()
+                .setAccept(ContentType.XML)
+                .setRelaxedHTTPSValidation()
+                .addHeader("custom header2", "header2.value")
+                .addQueryParam("requestID", new Random().nextLong())
+                .setBaseUri(SpellerConstants.YANDEX_SPELLER_API_URI)
+                .build();
+   }
 }
