12a74107e3e56b60ec03ae8893769636e4c5fe36
owl-se <alloc89@mail.ru>
2019-04-18 15:15:50 +0400

    go on

--- src/main/java/speller/core/YandexSpellerApi.java
+++ src/main/java/speller/core/YandexSpellerApi.java
@@ -1,7 +1,13 @@
 package speller.core;
 
+import java.util.HashMap;
+
 public class YandexSpellerApi {
 
     private YandexSpellerApi() {}
+
+    private HashMap<String, String> params = new HashMap<String, String>();
+
+    
 
 }
